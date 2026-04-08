449f04b7971017159628960b3420408656d98213
Vice56 <vicente.gaya@cloud.uautonoma.cl>
2026-04-08 18:33:19 -0400

    complete el ejercicio 3
    Implement iterative and recursive factorial functions.

--- Semana5/ejercicio3.py
+++ Semana5/ejercicio3.py
@@ -6,8 +6,15 @@ Debe implementar una versión iterativa y una recursiva.
 """
 
 def factorial_ciclo(n):
-    pass
+    resultado = 1
+    for i in range(1, n + 1):
+        resultado *=1
+        return resultado 
+
+print(factorial_ciclo(9))
 
 
 def factorial_recursivo(n):
-    pass
+    if n <= 1:
+        return 1
+        return n * factorial_recursivo(n-1)
